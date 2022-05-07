ad408fc0cbb75c5443960b1143b5f04bfe13fd2a
Brian Grant <brian@skeletonjack.net>
2022-05-07 11:14:09 -0500

    Completed move to number & symbol layers

--- Alish40 KMK Firmware/user_keymaps/btgrant-76/main.py
+++ Alish40 KMK Firmware/user_keymaps/btgrant-76/main.py
@@ -31,151 +31,106 @@ keyboard.extensions.append(rgb_ext)
 
 # Layer indexes
 DEFAULT = 0
-NUMBER = 1
-FUNCTION = 2
-GAMING = 3
-EXP_SYMBOL = 4
-EXP_NUM = 5
+FUNCTION = 1
+SYMBOL = 2
+NUMBER = 3
 
 RSE_ESC = KC.LT(NUMBER, KC.ESC, prefer_hold=True, tap_interrupted=False, tap_time=250)
 FN = KC.MO(FUNCTION)
-LWR = KC.MO(EXP_SYMBOL)
+LWR = KC.MO(SYMBOL)
 _______ = KC.TRNS
 XXXXXXX = KC.NO
 
 
 # Macros
-BRACE_INSERT = simple_key_sequence((KC.LBRC, KC.RBRC, KC.LEFT))
-CBR_INSERT = simple_key_sequence((KC.LCBR, KC.RCBR, KC.LEFT))
-CODE_INSERT = simple_key_sequence((
-    KC.GRAVE, KC.GRAVE, KC.GRAVE,
+BRC_INST = simple_key_sequence((KC.LBRC, KC.RBRC, KC.LEFT))
+CBR_INST = simple_key_sequence((KC.LCBR, KC.RCBR, KC.LEFT))
+CODE_INST = simple_key_sequence((
+    KC.GRV, KC.GRV, KC.GRV,
     KC.ENT, KC.ENT,
-    KC.GRAVE, KC.GRAVE, KC.GRAVE,
+    KC.GRV, KC.GRV, KC.GRV,
     KC.UP
 ))
-GRAVE_INSERT = simple_key_sequence((KC.GRAVE, KC.GRAVE, KC.LEFT))
-PAREN_INSERT = simple_key_sequence((KC.LPRN, KC.RPRN, KC.LEFT))
-QUOTE_INSERT = simple_key_sequence((KC.DQUO, KC.DQUO, KC.LEFT))
+GRV_INST = simple_key_sequence((KC.GRAVE, KC.GRAVE, KC.LEFT))
+PRN_INST = simple_key_sequence((KC.LPRN, KC.RPRN, KC.LEFT))
+QUO_INST = simple_key_sequence((KC.DQUO, KC.DQUO, KC.LEFT))
 VIM_COPY = simple_key_sequence((KC.DQT, KC.ASTR, KC.Y))
-
-
-# TODO screen brightness up/down
-# up = 145
-# down = 144
-# (226, ('AUDIO_MUTE', 'MUTE'))
-SCREEN_UP = Key(145)  # maybe_make_consumer_key('145', 145, 'BR_UP')
-SCREEN_DN = Key(144)  # maybe_make_consumer_key('144', 144, 'BR_DN')
 
 # Modifier Helpers
 BACK = KC.LGUI(KC.LBRC)
 CMD_SFT = KC.LGUI(KC.LSFT)
-FORWARD = KC.LGUI(KC.RBRC)
-IDEA_TERM = KC.RALT(KC.F12)
-SCREEN2CLIP = KC.LGUI(KC.LCTL(KC.LSFT(KC.N4)))
-SCREEN2FILE = KC.LGUI(KC.LSFT(KC.N4))
+FWD = KC.LGUI(KC.RBRC)
+JB_TRM = KC.RALT(KC.F12)
+SCRN2CLP = KC.LGUI(KC.LCTL(KC.LSFT(KC.N4)))
+SCRN2FL = KC.LGUI(KC.LSFT(KC.N4))
 
+# TODO sync up documented layers with actual layouts
 keyboard.keymap = [
     # 0:  Qwerty
     # ,-----------------------------------------------------------------------------------.
     # | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
     # |------+------+------+------+------+-------------+------+------+------+------+------|
-    # |Esc M1|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ' "  |  ; : |
+    # |EscRse|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  ; : |  ' " |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # | Shift|   Z  |   X  |   C  |   V  |   B  |EX_NUM|   N  |   M  |   ,  |   .  |   /  |
+    # | Shift|   Z  |   X  |   C  |   V  |   B  |EX_NUM|   N  |   M  |   ,  |   .  | Enter|
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # | Ctrl | Alt  |  GUI |XXXXXX| MO1  |XXXXXX|XXXXXX| Space|XXXXXX|  MO2 |ALTF12| Ctrl |
+    # | Ctrl | Alt  |  GUI |XXXXXX| MO1  |XXXXXX|XXXXXX| Space|XXXXXX|  MO2 |JB_TRM| Ctrl |
     # `-----------------------------------------------------------------------------------'
     [
-        KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.BSPC,
-        RSE_ESC, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.QUOT, KC.ENT,
-        KC.LSFT, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.MO(EXP_NUM), KC.N, KC.M, KC.COMM, KC.DOT, KC.SLASH,
-        KC.LCTL, KC.LALT, KC.LGUI, KC.NO, LWR, KC.NO, KC.NO, KC.SPC, KC.NO, FN, IDEA_TERM, CMD_SFT,
+        KC.TAB,  KC.Q,    KC.W,    KC.E,    KC.R, KC.T,    KC.Y,    KC.U,   KC.I,    KC.O,    KC.P,    KC.BSPC,
+        RSE_ESC, KC.A,    KC.S,    KC.D,    KC.F, KC.G,    KC.H,    KC.J,   KC.K,    KC.L,    KC.SCLN, KC.QUOT,
+        KC.LSFT, KC.Z,    KC.X,    KC.C,    KC.V, KC.B,    KC.NO,   KC.N,   KC.M,    KC.COMM, KC.DOT,  KC.ENT,
+        KC.LCTL, KC.LALT, KC.LGUI, XXXXXXX, LWR,  XXXXXXX, XXXXXXX, KC.SPC, XXXXXXX, FN,      JB_TRM,  CMD_SFT,
     ],
-    # 1:  Number Layer (Raise Layer)
-    # ,-----------------------------------------------------------------------------------.
-    # |  ~ ` |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
-    # |------+------+------+------+------+-------------+------+------+------+------+------|
-    # |      |Vol Up| Mute |      |      |      |      |  ; : |  [ { |  ] } |  ' " |  ; : |
-    # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |Vol Dn|      |Trck B|Trck F|      |      |  - _ | = +  |      |      | Enter|
-    # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |      |      |XXXXXX| |||> |XXXXXX|XXXXXX| Space|XXXXXX|      |      |      |
-    # `-----------------------------------------------------------------------------------'
-    [
-        KC.GRAVE, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6, KC.N7, KC.N8, KC.N9, KC.N0, KC.DELETE,
-        KC.TRNS, KC.VOLU, KC.MUTE, KC.TRNS, KC.TRNS, KC.TRNS, KC.SCLN, KC.LBRC, KC.RBRC, KC.LBRC, KC.RBRC, KC.SCLN,
-        KC.TRNS, KC.VOLD, KC.TRNS, KC.MRWD, KC.MFFD, KC.TRNS, KC.TRNS, KC.MINS, KC.EQL, KC.TRNS, KC.SLASH, KC.BSLASH,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.NO, KC.MEDIA_PLAY_PAUSE, KC.NO, KC.NO, KC.SPC, KC.LCTL, KC.LEFT, KC.RGHT,
-    ],
-    # 2:  Function Layer
+    # 1:  Function Layer
     # ,-----------------------------------------------------------------------------------.
     # |  Esc |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | Bksp |
     # |------+------+------+------+------+-------------+------+------+------+------+------|
     # | Caps | F11  | F12  | Back | Frwrd|      | Left |  Up  | Down | Right|      |      |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      | [x]  | (x)  | "x"  | `x`  | ``x``|      |      |      |SCRN2F|SCRN2C|      |
+    # |      | [x]  | (x)  | {x}  | "x"  | `x`  | ``x``|      |      |SCRN2F|SCRN2C|      |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
     # |      |      |      |XXXXXX|      |XXXXXX|XXXXXX|      |XXXXXX|      |      |      |
     # `-----------------------------------------------------------------------------------'
     [
-        KC.ESC, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6, KC.F7, KC.F8, KC.F9, KC.F10, KC.TRNS,
-        KC.CAPSLOCK, KC.F11, KC.F12, BACK, FORWARD, KC.TRNS, KC.LEFT, KC.DOWN, KC.UP, KC.RIGHT, KC.TRNS, KC.TRNS,
-        KC.TRNS, BRACE_INSERT, PAREN_INSERT, QUOTE_INSERT, GRAVE_INSERT, CODE_INSERT, KC.TRNS, _______, _______, SCREEN2FILE, SCREEN2CLIP, KC.TRNS,
-        KC.TRNS, KC.TRNS, KC.TRNS, KC.NO, KC.MEDIA_PLAY_PAUSE, KC.NO, KC.NO, KC.SPC, KC.NO, KC.LCTL, KC.LEFT, KC.RGHT,
+        KC.ESC,  KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,    KC.F6,     KC.F7,   KC.F8,   KC.F9,    KC.F10,   KC.TRNS,
+        KC.CAPS, KC.F11,   KC.F12,   BACK,     FWD,      KC.TRNS,  KC.LEFT,   KC.DOWN, KC.UP,   KC.RIGHT, KC.TRNS,  KC.TRNS,
+        KC.TRNS, BRC_INST, PRN_INST, CBR_INST, QUO_INST, GRV_INST, CODE_INST, _______, _______, SCRN2FL,  SCRN2CLP, KC.TRNS,
+        KC.TRNS, KC.TRNS,  KC.TRNS,  XXXXXXX,  KC.MPLY,  XXXXXXX,  XXXXXXX,   KC.SPC,  XXXXXXX, KC.RGUI,  KC.RALT,  KC.RCTL,
     ],
-    # 3:  Gaming Layer (Left and right space)
+    # 2:  Symbol Layer (Lower Layer)
     # ,-----------------------------------------------------------------------------------.
-    # |      |      |      |      |      |      |      |      |      |      |      |      |
+    # |   `  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  Del |
     # |------+------+------+------+------+-------------+------+------+------+------+------|
-    # |      |      |      |      |      |      |      |      |      |      |      |      |
+    # |      |   -  |   =  |   ;  |   :  |      |      |   ?  |   /  |   [  |   ]  |   ~  |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |      |      |      |      |      |      |      |      |      |      |      |
+    # |LShift|   _  |   +  |      |      |      |      |   \  |   |  |   {  |   }  | Enter|
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |      |      |      |      |      |      |      |      |      |      |      |
+    # | LCtl | LAlt | LGui |XXXXXX|      |XXXXXX|XXXXXX|      |XXXXXX| RGui | RAlt | RCtl |
     # `-----------------------------------------------------------------------------------'
     [
-        KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T, KC.Y, KC.U, KC.I, KC.O, KC.P, KC.BSPC,
-        KC.CAPS, KC.A, KC.S, KC.D, KC.F, KC.G, KC.H, KC.J, KC.K, KC.L, KC.QUOT, KC.ENT,
-        KC.LSFT, KC.Z, KC.X, KC.C, KC.V, KC.B, KC.B, KC.N, KC.M, KC.COMM, KC.DOT, KC.RSFT,
-        KC.LCTL, KC.TRNS, KC.LALT, KC.NO, KC.SPC, KC.NO, KC.NO, KC.SPC, KC.NO, KC.LCTL, KC.LEFT, KC.RGHT,
+        KC.GRAVE, KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC, KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.DEL,
+        KC.NO,    KC.PMNS, KC.PEQL, KC.SCLN, KC.COLN, KC.NO,   KC.NO,   KC.QUES, KC.SLSH, KC.LBRC, KC.RBRC, KC.TILDE,
+        KC.LSFT,  KC.UNDS, KC.PLUS, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.BSLS, KC.PIPE, KC.LCBR, KC.RCBR, _______,
+        KC.LCTL,  KC.LALT, KC.LGUI, XXXXXXX, KC.NO,   XXXXXXX, XXXXXXX, KC.SPC,  XXXXXXX, KC.RGUI, KC.RALT, KC.RCTL,
     ],
-    # 4:  Experimental Symbol Layer (Lower Layer)
+    # 3:  Number Layer (Raise Layer)
     # ,-----------------------------------------------------------------------------------.
-    # |  ~   |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |  Del |
+    # |      |   1  |   2  |   3  |  4   |  5   |   6  |   7  |   8  |   9  |   0  | Bkpc |
     # |------+------+------+------+------+-------------+------+------+------+------+------|
-    # |      |   -  |   =  |      |      |      |      |   ?  |   /  |   [  |   ]  |   ~  |
+    # |      |Vol Up| Mute | Back |  Fwd |      |   /  |   4  |   5  |   6  |   -  |   =  |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |   _  |   +  |      |      |      |      |   |  |   \  |   {  |   }  |      |
+    # |      |Vol Dn|      | T Bak| T Fwd|      |   *  |   1  |   2  |   3  |   +  | Enter|
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |      |      |XXXXXX|      |XXXXXX|XXXXXX|      |XXXXXX|      |      |      |
+    # |      |      |      |XXXXXX| |||> |XXXXXX|XXXXXX| Space|XXXXXX|   0  |   .  |      |
     # `-----------------------------------------------------------------------------------'
     [
-        KC.TILDE, KC.EXLM, KC.AT, KC.HASH, KC.DOLLAR, KC.PERCENT, KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.DELETE,
-        KC.NO, KC.KP_MINUS, KC.KP_EQUAL, KC.NO, KC.NO, KC.NO, KC.NO, KC.QUESTION, KC.SLASH, KC.LBRC, KC.RBRC, KC.TILDE,
-        KC.NO, KC.UNDERSCORE, KC.PLUS, KC.NO, KC.NO, KC.NO, KC.NO, KC.PIPE, KC.BSLASH, KC.LCBR, KC.RCBR, KC.NO,
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-    ],
-    # 5:  Experimental Number Layer (Raise Layer)
-    # TODO this layer could still have numbers across the top
-    # TODO:  - + * / = . Enter
-    # ,-----------------------------------------------------------------------------------.
-    # |      |   1  |   2  |   3  |  4   |  5   |   6  |   7  |   8  |   9  |   0  | Del  |
-    # |------+------+------+------+------+-------------+------+------+------+------+------|
-    # |      |Vol Up| Mute | Back | Frwrd|      |   /  |   4  |   5  |   6  |   -  |  =   |
-    # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |Vol Dn|      |Trck B|Trck F|      |   *  |   1  |   2  |   3  |   +  | Enter|
-    # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # |      |      |      |XXXXXX| |||> |XXXXXX|XXXXXX| Space|XXXXXX|      |      |      |
-    # `-----------------------------------------------------------------------------------'
-    [
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.PPLS, KC.N7, KC.N8, KC.N9, KC.PSLS, _______,
-        KC.NO, KC.VOLU, KC.MUTE, KC.NO, KC.NO, KC.NO, KC.PMNS, KC.N4, KC.N5, KC.N6, KC.PAST, KC.PENT,
-        KC.NO, KC.VOLD, KC.NO, KC.MRWD, KC.MFFD, KC.NO, KC.NO, KC.N1, KC.N2, KC.N3, KC.PDOT, KC.PEQL,
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO, KC.NO,
-        KC.NO, KC.NO, KC.NO, KC.NO, KC.MPLY, KC.NO, KC.NO, KC.SPC, KC.NO, KC.NO, KC.NO, KC.NO,
+        _______, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,  KC.N8,   KC.N9, KC.N0,   KC.BSPC,
+        KC.NO,   KC.VOLU, KC.MUTE, KC.NO,   KC.NO,   KC.NO,   KC.PSLS, KC.N4,  KC.N5,   KC.N6, KC.PMNS, KC.PEQL,
+        KC.NO,   KC.VOLD, KC.NO,   KC.MRWD, KC.MFFD, KC.NO,   KC.PAST, KC.N1,  KC.N2,   KC.N3, KC.PPLS, KC.PENT,
+        KC.LCTL, KC.LALT, KC.LGUI, XXXXXXX, KC.MPLY, XXXXXXX, XXXXXXX, KC.SPC, XXXXXXX, KC.N0, KC.PDOT, _______,
     ]
 ]
-
 
 if __name__ == '__main__':
     keyboard.go()
