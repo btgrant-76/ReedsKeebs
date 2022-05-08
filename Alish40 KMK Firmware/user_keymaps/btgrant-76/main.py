cea475a84ee1fa0dd35bce69f401d68a6fb0e089
Brian Grant <brian@skeletonjack.net>
2022-05-08 08:50:34 -0500

    Sync Mercutio & Alish 40

--- Alish40 KMK Firmware/user_keymaps/btgrant-76/main.py
+++ Alish40 KMK Firmware/user_keymaps/btgrant-76/main.py
@@ -72,7 +72,7 @@ keyboard.keymap = [
     # |------+------+------+------+------+-------------+------+------+------+------+------|
     # |EscRse|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  ; : |  ' " |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
-    # | Shift|   Z  |   X  |   C  |   V  |   B  |EX_NUM|   N  |   M  |   ,  |   .  | Enter|
+    # | Shift|   Z  |   X  |   C  |   V  |   B  |XXXXXX|   N  |   M  |   ,  |   .  | Enter|
     # |------+------+------+------+------+------+------+------+------+------+------+------|
     # | Ctrl | Alt  |  GUI |XXXXXX| MO1  |XXXXXX|XXXXXX| Space|XXXXXX|  MO2 |JB_TRM| Ctrl |
     # `-----------------------------------------------------------------------------------'
@@ -84,7 +84,7 @@ keyboard.keymap = [
     ],
     # 1:  Function Layer
     # ,-----------------------------------------------------------------------------------.
-    # |  Esc |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | Bksp |
+    # |  Esc |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  |  Del |
     # |------+------+------+------+------+-------------+------+------+------+------+------|
     # | Caps | F11  | F12  | Back | Frwrd|      | Left |  Up  | Down | Right|      |      |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -93,7 +93,7 @@ keyboard.keymap = [
     # |      |      |      |XXXXXX|      |XXXXXX|XXXXXX|      |XXXXXX|      |      |      |
     # `-----------------------------------------------------------------------------------'
     [
-        KC.ESC,  KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,    KC.F6,     KC.F7,   KC.F8,   KC.F9,    KC.F10,   KC.TRNS,
+        KC.ESC,  KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,    KC.F6,     KC.F7,   KC.F8,   KC.F9,    KC.F10,   KC.DEL,
         KC.CAPS, KC.F11,   KC.F12,   BACK,     FWD,      KC.TRNS,  KC.LEFT,   KC.DOWN, KC.UP,   KC.RIGHT, KC.TRNS,  KC.TRNS,
         KC.TRNS, BRC_INST, PRN_INST, CBR_INST, QUO_INST, GRV_INST, CODE_INST, _______, _______, SCRN2FL,  SCRN2CLP, KC.TRNS,
         KC.TRNS, KC.TRNS,  KC.TRNS,  XXXXXXX,  KC.MPLY,  XXXXXXX,  XXXXXXX,   KC.SPC,  XXXXXXX, KC.RGUI,  KC.RALT,  KC.RCTL,
@@ -111,12 +111,12 @@ keyboard.keymap = [
     [
         KC.GRAVE, KC.EXLM, KC.AT,   KC.HASH, KC.DLR,  KC.PERC, KC.CIRC, KC.AMPR, KC.ASTR, KC.LPRN, KC.RPRN, KC.DEL,
         KC.NO,    KC.PMNS, KC.PEQL, KC.SCLN, KC.COLN, KC.NO,   KC.NO,   KC.QUES, KC.SLSH, KC.LBRC, KC.RBRC, KC.TILDE,
-        KC.LSFT,  KC.UNDS, KC.PLUS, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.BSLS, KC.PIPE, KC.LCBR, KC.RCBR, _______,
+        KC.LSFT,  KC.UNDS, KC.PLUS, KC.NO,   KC.NO,   KC.NO,   KC.NO,   KC.PIPE, KC.BSLS, KC.LCBR, KC.RCBR, _______,
         KC.LCTL,  KC.LALT, KC.LGUI, XXXXXXX, KC.NO,   XXXXXXX, XXXXXXX, KC.SPC,  XXXXXXX, KC.RGUI, KC.RALT, KC.RCTL,
     ],
     # 3:  Number Layer (Raise Layer)
     # ,-----------------------------------------------------------------------------------.
-    # |      |   1  |   2  |   3  |  4   |  5   |   6  |   7  |   8  |   9  |   0  | Bkpc |
+    # |   `  |   1  |   2  |   3  |  4   |  5   |   6  |   7  |   8  |   9  |   0  | Bkpc |
     # |------+------+------+------+------+-------------+------+------+------+------+------|
     # |      |Vol Up| Mute | Back |  Fwd |      |   /  |   4  |   5  |   6  |   -  |   =  |
     # |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -125,10 +125,10 @@ keyboard.keymap = [
     # |      |      |      |XXXXXX| |||> |XXXXXX|XXXXXX| Space|XXXXXX|   0  |   .  |      |
     # `-----------------------------------------------------------------------------------'
     [
-        _______, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,  KC.N8,   KC.N9, KC.N0,   KC.BSPC,
-        KC.NO,   KC.VOLU, KC.MUTE, KC.NO,   KC.NO,   KC.NO,   KC.PSLS, KC.N4,  KC.N5,   KC.N6, KC.PMNS, KC.PEQL,
-        KC.NO,   KC.VOLD, KC.NO,   KC.MRWD, KC.MFFD, KC.NO,   KC.PAST, KC.N1,  KC.N2,   KC.N3, KC.PPLS, KC.PENT,
-        KC.LCTL, KC.LALT, KC.LGUI, XXXXXXX, KC.MPLY, XXXXXXX, XXXXXXX, KC.SPC, XXXXXXX, KC.N0, KC.PDOT, _______,
+        KC.GRAVE, KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,   KC.N6,   KC.N7,  KC.N8,   KC.N9, KC.N0,   KC.BSPC,
+        KC.NO,    KC.VOLU, KC.MUTE, KC.NO,   KC.NO,   KC.NO,   KC.PSLS, KC.N4,  KC.N5,   KC.N6, KC.PMNS, KC.PEQL,
+        KC.NO,    KC.VOLD, KC.NO,   KC.MRWD, KC.MFFD, KC.NO,   KC.PAST, KC.N1,  KC.N2,   KC.N3, KC.PPLS, KC.PENT,
+        KC.LCTL,  KC.LALT, KC.LGUI, XXXXXXX, KC.MPLY, XXXXXXX, XXXXXXX, KC.SPC, XXXXXXX, KC.N0, KC.PDOT, _______,
     ]
 ]
 
